c6f0a096c7f1cb8cb1383fe5135b5a8e63f5a141
Kelly Online <Kellyonline2016@gmail.com>
2019-03-15 22:08:34 +0100

    Introduction to Csharp and Dotnet

---
+++ Day1/AssignmentDay1.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Loop{
+
+  public static void Main(){
+
+    for(int i = 1; i <= 20; i++){
+
+      //Console.Write(i);
+      Console.WriteLine(i);
+    }
+  }
+}
---
+++ Day1/Day1.cs
@@ -0,0 +1,35 @@
+using System;
+
+
+public class Test{
+
+  public static void Mai(){
+
+    // strings
+    // integers
+    // Decimal
+    // Floats
+    // List, Ilist, IEnumerables
+    // For Loop
+    // Switch
+    // Arrays
+    // For Each
+    // Enum
+
+
+string s1 = "GeeksForGeeks"; 
+   
+
+        Console.WriteLine("String : " + s1); 
+   
+        // totalwidth is less than string length 
+        Console.WriteLine("Pad  2 :'{0}'", s1.PadLeft(2)); 
+   
+        // totalwidth is equal to string length 
+        Console.WriteLine("Pad 13 :'{0}'", s1.PadLeft(13)); 
+   
+        // totalwidth is greater then string length 
+        Console.WriteLine("Pad 20 :'{0}'", s1.PadLeft(20));
+
+        }
+}
